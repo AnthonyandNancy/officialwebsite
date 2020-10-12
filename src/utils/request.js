cd8655e74831a7d898df69ad105170f8587cc0b5
郭建林 <1050070285@qq.com>
2020-10-12 11:59:54 +0800

    初始模板

---
+++ src/utils/request.js
@@ -0,0 +1,51 @@
+import axios from 'axios'
+// eslint-disable-next-line no-unused-vars
+import { MessageBox, Message } from 'element-ui'
+
+// create an axios instance
+const service = axios.create({
+  baseURL: 'https://api-hmugo-web.itheima.net/api/public/v1', // url = base url + request url
+  // withCredentials: true, // send cookies when cross-domain requests
+  timeout: 5000 // request timeout
+})
+
+// request interceptor
+service.interceptors.request.use(
+    config => {
+      return config
+    },
+  error => {
+    // do something with request error
+    console.log('error>>',error) // for debug
+    return Promise.reject(error)
+  }
+)
+
+// response interceptor
+service.interceptors.response.use(
+  /**
+   * If you want to get http information such as headers or status
+   * Please return  response => response
+  */
+
+  /**
+   * Determine the request status by custom code
+   * Here is just an example
+   * You can also judge the status by HTTP Status Code
+   */
+  response => {
+    const res = response.data
+    // console.log('response>>>>',response)
+    return res
+  },
+  error => {
+    Message({
+      message: error,
+      type: 'error',
+      duration: 5 * 1000
+    })
+    return Promise.reject(error)
+  }
+)
+
+export default service
