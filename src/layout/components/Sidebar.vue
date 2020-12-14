994c2d116b99eb6b03b087e61d7347c1383429f7
Anthony <1050070285@qq.com>
2020-12-14 18:37:33 +0800

    新增激活状态和显示隐藏

--- src/layout/components/Sidebar.vue
+++ src/layout/components/Sidebar.vue
@@ -2,21 +2,59 @@
 <template>
   <div>
     <!--    第一种方案-->
-        <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" :router="true" @select="handleSelect">
-          <template v-for="(item , index ) in routes">
-            <el-menu-item v-if="!item.children" :key="index" :index="String(index)" :route="item.path">{{ item.meta.title }}</el-menu-item>
+    <!-- <el-menu :default-active="$route.path" class="el-menu-demo" mode="horizontal" :router="true" @select="handleSelect">
+      <template v-for="(item , index ) in routes">
+        <el-menu-item v-if="!item.children" :key="index" :index="String(index)" :route="item.path">{{ item.meta.title }}</el-menu-item>
 
-            <el-submenu v-if="item.children" :key="index" :index="String(index)">
-              <template slot="title">{{ item.meta.title }}</template>
-              <template v-for="(itemChildren , indexChildren ) in item.children">
-                <el-menu-item :key="indexChildren" :index="resolveIndex(index,indexChildren)" :route="itemChildren.path">{{ itemChildren.meta.title }}</el-menu-item>
-              </template>
-
-            </el-submenu>
-
+        <el-submenu v-if="item.children" :key="index" :index="String(index)">
+          <template slot="title">{{ item.meta.title }}</template>
+          <template v-for="(itemChildren , indexChildren ) in item.children">
+            <el-menu-item :key="indexChildren" :index="resolveIndex(index,indexChildren)" :route="itemChildren.path">{{ itemChildren.meta.title }}</el-menu-item>
           </template>
 
-        </el-menu>
+        </el-submenu>
+
+      </template>
+
+    </el-menu> -->
+    <el-menu
+      :default-active="$route.path"
+      class="el-menu-demo mt-1"
+      mode="horizontal"
+      :router="true"
+      @select="handleSelect"
+    >
+      <template v-for="(item, index) in routes">
+        <el-menu-item
+          v-if="!item.children"
+          v-show="!item.hidden"
+          :key="index"
+          :index="item.path"
+          :route="item.path"
+        >
+          {{ item.meta.title }}
+          <!-- <svg-icon :icon-class="item.meta.icon" /> -->
+        </el-menu-item>
+
+        <el-submenu v-if="item.children" :key="index" :index="item.path">
+          <template slot="title">
+            {{ item.meta.title }}
+            <!-- <svg-icon :icon-class="item.meta.icon" /> -->
+          </template>
+          <el-menu-item-group>
+            <template v-for="(itemChildren, indexChildren) in item.children">
+              <el-menu-item
+                :key="indexChildren"
+                :index="resolveIndex(index, indexChildren)"
+                :route="itemChildren.path"
+              >
+                {{ itemChildren.meta.title }}
+              </el-menu-item>
+            </template>
+          </el-menu-item-group>
+        </el-submenu>
+      </template>
+    </el-menu>
   </div>
 </template>
 
